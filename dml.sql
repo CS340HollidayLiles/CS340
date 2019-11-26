89e2d1497a2c9c114fe767d8ea0b5f01b7ed435a
monicaholliday <hollidmo@oregonstate.edu>
2019-11-25 16:25:32 -0800

    Updated queries in dml.sql

--- dml.sql
+++ dml.sql
@@ -14,12 +14,12 @@ DELETE FROM guest WHERE guest_id = :guest_Id_from_update_form;
 SELECT * FROM guest;
 
 --Add Room
-INSERT INTO room (room_num, room_type, pets_allowed, max_guests, price) 
-  VALUES (:room_num_input, :room_type_input, :pets_allowed_input, :max_guests_input, price_input);
+INSERT INTO room (room_num, room_type, max_guests, price) 
+  VALUES (:room_num_input, :room_type_input, :max_guests_input, price_input);
 
 --Update Room
 UPDATE room 
-  SET room_num = :room_num_input, room_type = :room_type_input, pets_allowed = :pets_allowed_input, max_guests = :max_guests_input, price = :price_input 
+  SET room_type = :room_type_input, max_guests = :max_guests_input, price = :price_input 
   WHERE room_num = :room_num_input;
 
 --Delete Room
@@ -29,12 +29,12 @@ DELETE FROM room WHERE room_num = :room_num_from_update_form;
 SELECT * FROM room;
 
 --Add Reservation
-INSERT INTO reservation (guest_id, payment_id, room_num, check_in, check_out, bringing_pets, num_guests, confirmation_num, room_num)  
-  VALUES (:guest_id, :payment_id, :room_num, :check_in_input, :check_out_input, :bringing_pets_input, :num_guests_input, :generated_confirmation_num, :room_num_from_browse_rooms_form);
+INSERT INTO reservation (guest_id, payment_id, room_num, check_in, check_out, num_guests, confirmation_num)  
+  VALUES (:guest_id, :payment_id, :room_num, :check_in_input, :check_out_input, :num_guests_input, :generated_confirmation_num);
 
 --Update Reservation
 UPDATE reservation 
-  SET guest_id = :guest_id, payment_id = :payment_id, room_num = :room_num, check_in = :check_in_input, check_out = :check_out_input, num_guests = :num_guests_input, confirmation_num = :generated_confirmation_num, room_num = :room_num_from_browse_rooms_form 
+  SET guest_id = :guest_id, payment_id = :payment_id, room_num = :room_num, check_in = :check_in_input, check_out = :check_out_input, num_guests = :num_guests_input, confirmation_num = :generated_confirmation_num 
   WHERE reservation_id = :reservation_id_from_update_form;
 
 --Delete Reservation
@@ -44,12 +44,12 @@ DELETE FROM reservation WHERE reservation_id = :reservation_id_from_update_form;
 SELECT * FROM reservation;
 
 --Add Payment
-INSERT INTO payment (reservation_id, f_name, l_name, cc_num, cc_type, cc_security_code, house_num, street_name, city, state, zip_code, country, total_charged) 
+INSERT INTO payment (reservation_id, f_name, l_name, cc_num, cc_type, cc_security_code, house_num, street, city, state, zip_code, country, total_charged) 
   VALUES (:reservation_id, :f_name_input, :l_name_input, :cc_num_input, :cc_type_input, :cc_security_code_input, :house_num_input, :street_name_input, :city_input, :state_input, :zip_code_input, :country_input, :total_charged_from_reservation);
 
 --Update Payment
 UPDATE payment 
-  SET reservation_id = :reservation_id, f_name = :f_name_input, l_name = :l_name_input, cc_num = :cc_num_input, cc_type = :cc_type_input, cc_security_code = :cc_security_code_input, house_num = :house_num_input, street_name = :street_name_input, city = :city_input, state =:state_input, zip_code = :zip_code_input, country = :country_input, total_charge = :total_charged_from_reservation 
+  SET reservation_id = :reservation_id, f_name = :f_name_input, l_name = :l_name_input, cc_num = :cc_num_input, cc_type = :cc_type_input, cc_security_code = :cc_security_code_input, house_num = :house_num_input, street = :street_name_input, city = :city_input, state =:state_input, zip_code = :zip_code_input, country = :country_input, total_charged = :total_charged_from_reservation 
   WHERE payment_id = :payment_id_from_update_form;
 
 --Delete Payment
